28aa7fa6850b47087c92c0dcb8da7e7d4e075aa9
wangbochao <sergeywong@gmail.com>
2018-08-31 19:50:58 +0800

    add train processing for tom

--- train.py
+++ train.py
@@ -83,7 +83,59 @@ def train_gmm(opt, train_loader, model, board):
         loss = criterionL1(warped_cloth, im_c)
         visuals = [ [im_h, shape, im_pose], 
                    [c, warped_cloth, im_c], 
-                   [warped_grid, warped_mask,im ] ]
+                   [warped_grid, (warped_cloth+im)*0.5, im]]
+            
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+            
+        if (step+1) % opt.display_count == 0:
+            board_add_images(board, 'combine', visuals, step+1)
+            board.add_scalar('metric', loss.item(), step+1)
+            t = time.time() - iter_start_time
+            print('step: %8d, time: %.3f, loss: %4f' % (step+1, t, loss.item()))
+
+        if (step+1) % opt.save_count == 0:
+            save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'step_%06d.pth' % (step+1)))
+
+
+def train_tom(opt, train_loader, model, board):
+    # criterion
+    criterionL1 = nn.L1Loss()
+    criterionVGG = networks.VGGLoss()
+    criterionMask = lambda x: x.abs().mean() 
+    
+    # optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr, betas=(0.5, 0.999))
+    scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda = lambda step: 1.0 -
+            max(0, step - opt.keep_step) / float(opt.decay_step + 1))
+    
+    for step in range(opt.keep_step + opt.decay_step):
+        iter_start_time = time.time()
+        inputs = train_loader.next_batch()
+            
+        im = inputs['image'].cuda()
+        im_pose = inputs['pose_image']
+        im_h = inputs['head']
+        shape = inputs['shape']
+
+        agnostic = inputs['agnostic'].cuda()
+        c = inputs['cloth'].cuda()
+        cm = inputs['cloth_mask'].cuda()
+        
+        outputs = model(torch.cat([agnostic, c],1))
+        p_rendered, m_composite = torch.split(outputs, 3,1)
+        m_selected = m_composite * cm
+        p_tryon = c * m_selected+ p_rendered * (1 - m_selected)
+
+        loss_l1 = criterionL1(p_tryon, im)
+        loss_vgg = criterionVGG(p_tryon, im)
+        loss_mask = criterionMask(1 - m_composite)
+        loss = loss_l1 + loss_vgg + loss_mask
+
+        visuals = [ [im_h, shape, im_pose], 
+                   [c, cm, m_composite], 
+                   [p_rendered, p_tryon, im]]
             
         optimizer.zero_grad()
         loss.backward()
@@ -110,25 +162,27 @@ def main():
     # create dataloader
     train_loader = CPDataLoader(opt, train_dataset)
 
-    # create model
-    if opt.stage:
-        model = GMM(opt)
-    else:
-        model = UnetGenerator(25, 4, 6, ngf=64, norm_layer=nn.InstanceNorm2d)
-   
-    model.cuda()
-    model.train()
-    
     # visualization
     if not os.path.exists(opt.tensorboard_dir):
         os.makedirs(opt.tensorboard_dir)
     board = SummaryWriter(log_dir = os.path.join(opt.tensorboard_dir, opt.name))
    
-    # train
-    train_gmm(opt, train_loader, model, board)
+    # create model & train
+    if opt.stage:
+        model = GMM(opt)
+        model.cuda()
+        mode.train()
+        train_gmm(opt, train_loader, model, board)
+    else:
+        model = UnetGenerator(25, 4, 6, ngf=64, norm_layer=nn.InstanceNorm2d)
+        model.cuda()
+        mode.train()
+        train_tom(opt, train_loader, model, board)
+  
+    # save the final model checkpoint
     save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'gmm_final.pth'))
-    print('Finished traing %s' % (opt.stage))
+    print('Finished traing %s, nameed: %s!' % (opt.stage, opt.name))
 
 if __name__ == "__main__":
-    print("Start to train geometric matching module!")
+    print("Start to train stage: %s, named: %s!" % (opt.stage, opt.name))
     main()
